584b5c05cc31243d6b6e2021a530eb055b67bce0
Orator <orator9x@gmail.com>
2023-03-05 22:02:45 +0100

    Mirroring message

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ class CircusAchievementBot(discord.Client):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        await message.channel.send(f"Hi there! (Mirroring {message})")
 
 
 intents = discord.Intents.default()
